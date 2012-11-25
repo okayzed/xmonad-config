3915b8440b8edc7a44007fec12f5e70745a9ee12
okay <okay@fb.com>
2012-11-25 13:06:54 -0800

    Updates to xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -1,13 +1,23 @@
 import XMonad hiding ( (|||) )
 
+import XMonad.Actions.CycleWS
+import XMonad.Actions.DwmPromote
+import XMonad.Actions.TopicSpace
+
 import XMonad.Config.Bluetile
 import XMonad.Config.Gnome
-import XMonad.Util.Replace
-import XMonad.Util.Themes
-import XMonad.Util.EZConfig(additionalKeys)
+
+import XMonad.Hooks.CurrentWorkspaceOnTop
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.FadeInactive
-
-
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.Minimize
+import XMonad.Hooks.PositionStoreHooks
+import XMonad.Hooks.Script
+import XMonad.Hooks.ServerMode
+import XMonad.Hooks.WorkspaceByPos
 
 import XMonad.Layout.BorderResize
 import XMonad.Layout.BoringWindows
@@ -24,28 +34,90 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.PositionStoreFloat
 import XMonad.Layout.WindowSwitcherDecoration
 
-import XMonad.Layout.Cross
-import XMonad.Layout.Circle
+import XMonad.Prompt
+import XMonad.Prompt.Shell
+import XMonad.Prompt.Window
+import XMonad.Prompt.Workspace
+import XMonad.Prompt.XMonad
 
-import XMonad.Hooks.CurrentWorkspaceOnTop
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.PositionStoreHooks
-import XMonad.Hooks.Minimize
-import XMonad.Hooks.ServerMode
-import XMonad.Hooks.WorkspaceByPos
-import XMonad.Layout.DwmStyle
+import XMonad.Util.EZConfig
+import XMonad.Util.Replace
+import XMonad.Util.Themes
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+
+
 
 myManageHook = composeAll [
   resource =? "desktop_window" --> doIgnore
   , className =? "synapse" --> doIgnore
   ]
-
 myDragger = BordersDragger
 
+-- The list of all topics/workspaces of your xmonad configuration.
+-- The order is important, new topics must be inserted
+-- at the end of the list if you want hot-restarting
+-- to work.
+myTopics :: [Topic]
+myTopics =
+  [ "web" -- the first one
+  , "music"
+  , "chrome"
+  , "mail"
+  , "vim"
+  , "todo"
+  , "top"
+  ]
+
+myTopicConfig :: TopicConfig
+myTopicConfig = defaultTopicConfig
+  { topicDirs = M.fromList $
+      [ ("conf", "w/conf")
+      , ("music", "Music")
+      , ("terminal", "w/tonka/src")
+      ]
+  , defaultTopicAction = const $ spawnShell >*> 3
+  , defaultTopic = "dashboard"
+  , topicActions = M.fromList $
+      [ ("conf",       spawnShell >> spawnShellIn ".xmonad")
+      , ("terminal",   spawnShellIn "wd" >>
+                       spawnShellIn "wd" >>
+                       spawnShellIn "wd")
+      , ("mail",       mailAction)
+      , ("irc",        spawnShell)
+      , ("vim",        spawnVimIn "wd")
+      , ("todo",       spawnVimIn "wd")
+      , ("dashboard",  spawnShell)
+      , ("web",        browserCmd)
+      ]
+  }
+
+-- extend your keybindings
+spawnShell :: X ()
+spawnShell = currentTopicDir myTopicConfig >>= spawnShellIn
+
+mailAction = spawn $ "thunderbird"
+browserCmd = spawn $ "xdg-open http://google.com"
+myShell = "bash"
+
+spawnShellIn :: Dir -> X ()
+spawnShellIn dir = spawn $ "urxvt '(cd ''" ++ dir ++ "'' && " ++ myShell ++ " )'"
+
+spawnVimIn :: Dir -> X ()
+spawnVimIn dir = spawn $ "urxvt '(cd ''" ++ dir ++ "'' && " ++ myShell ++ " && vim)'"
+
+goto :: Topic -> X ()
+goto = switchTopic myTopicConfig
+
+promptedGoto :: X ()
+promptedGoto = workspacePrompt defaultXPConfig goto
+
+promptedShift :: X ()
+promptedShift = workspacePrompt defaultXPConfig $ windows . W.shift
 
 bluetileLayoutHook = avoidStruts $ minimize $ boringWindows $ (
+    named "Dashboard" tiled2 |||
     named "Floating" floating |||
     named "Tiled1" tiled1 |||
     named "Tiled2" tiled2 |||
@@ -61,18 +133,27 @@ bluetileLayoutHook = avoidStruts $ minimize $ boringWindows $ (
     }
     fullscreen = maximize $ smartBorders Full
 
-myLogHook = fadeInactiveLogHook fadeAmount
-      where fadeAmount = 0.9
+myStartup = do
+  spawn "bash ~/.xinitrc"
 
-
-main = xmonad $ bluetileConfig
+myConfig = bluetileConfig
   { borderWidth = 2
     , normalBorderColor  = "#000" -- "#dddddd"
     , focusedBorderColor = "#999"    -- "#ff0000" don't use hex, not <24 bit safe
-    , manageHook = manageHook bluetileConfig <+> myManageHook
+    , manageHook = manageHook gnomeConfig <+> myManageHook
     , focusFollowsMouse  = True
     , layoutHook = bluetileLayoutHook
-    , logHook = myLogHook
-  } `additionalKeys`
-  [ ((mod4Mask, xK_p),    spawn "dmenu_run -b -nb black -nf white") --call dmenu
+    , startupHook = ewmhDesktopsStartup <+> myStartup
+    , workspaces = myTopics } 
+  `additionalKeys` [
+    ((mod4Mask, xK_p),    spawn "dmenu_run -b -nb black -nf white") --call dmenu
+  , ((mod4Mask, xK_o), moveTo Prev NonEmptyWS)
+  , ((mod4Mask, xK_i), moveTo Next NonEmptyWS)
+  , ((mod4Mask, xK_space), dwmpromote)
+  , ((mod4Mask              , xK_n     ), windowPromptGoto defaultXPConfig)
+  , ((mod4Mask .|. shiftMask, xK_n     ), windowPromptBring defaultXPConfig)
+  , ((mod4Mask              , xK_g     ), promptedGoto)
+  , ((mod4Mask .|. shiftMask, xK_g     ), promptedShift)
   ]
+
+main = xmonad =<< xmobar myConfig
